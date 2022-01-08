81cfb53bd0de2fdead0f3fd5166cdf53b60a703b
rzp <renzhipeng123@163.com>
2022-01-08 18:59:10 +0800

    feat✨:  title, keywords, etc in head

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,10 @@
 // you can use this to manipulate the document head in any components,
 // they will be rendered correctly in the html results with vite-ssg
 useHead({
-  title: 'Vitesse',
+  title: 'Aaron\'s Blog',
   meta: [
-    { name: 'description', content: 'Opinionated Vite Starter Template' },
+    { name: 'keywords', content: 'Aaron, Aaron\'s Blog, blog' },
+    { name: 'description', content: 'Aaron blog that recorded daily life and technology' },
   ],
 })
 </script>
